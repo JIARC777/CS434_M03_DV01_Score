93eb5d69118b130e4c417395c367b8eeb5a35cf9
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-02-10 12:27:40 -0700

    Build

---
+++ JI_M01_P01_v01/Assets/Scripts/LightModifier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightModifier : MonoBehaviour
+{
+    Light directionalLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score score = FindObjectOfType<Score>();
+        directionalLight = this.GetComponent<Light>();
+        score.OnMilestone += changeLightColor;
+    }
+
+
+    public void changeLightColor()
+    {
+        directionalLight.color = Random.ColorHSV();
+    }
+}
